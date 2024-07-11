ce37b33942e70dd7bb3e3bbaec3be73b560f465e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-07-11 17:30:26 +0200

    spring initializer

---
+++ src/main/java/example/lohnsoftware/LohnsoftwareApplication.java
@@ -0,0 +1,13 @@
+package example.lohnsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LohnsoftwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LohnsoftwareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/lohnsoftware/LohnsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package example.lohnsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LohnsoftwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
