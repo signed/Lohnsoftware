6be78eb893f451cefb6d6bb9bbd856bf5a19d388
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-07-15 21:35:32 +0200

    verschiebe in Produktionscode

--- src/main/java/example/lohnsoftware/infrastructure/JsonArbeitszeitkonto.java
+++ src/main/java/example/lohnsoftware/infrastructure/JsonArbeitszeitkonto.java
@@ -3,7 +3,18 @@ package example.lohnsoftware.infrastructure;
 import example.lohnsoftware.core.Arbeitszeitkonto;
 import example.lohnsoftware.core.MonatsArbeitsstunden;
 
+import java.nio.file.Path;
+import java.util.Locale;
+
 public class JsonArbeitszeitkonto implements Arbeitszeitkonto {
+    public static Path relativerPfadZumSpeicherort(MonatsArbeitsstunden monatsArbeitsstunden) {
+        final var localMonth = monatsArbeitsstunden.month();
+        final var jahrOrdner = localMonth.year().toString();
+        final var monatsOrdner = String.format(Locale.US, "%02d", localMonth.month().getValue());
+        final var dateiName = monatsArbeitsstunden.mitarbeiter().nummer() + ".json";
+        return Path.of(jahrOrdner, monatsOrdner, dateiName);
+    }
+
     @Override
     public Ergebnis erfasse(MonatsArbeitsstunden monatsArbeitsstunden) {
         return Ergebnis.ergebnis("");
