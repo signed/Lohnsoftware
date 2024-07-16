1ad19bd0c8a734e46fcc14cafd6530c99d5b08bb
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-07-16 12:00:10 +0200

    cleanup

--- src/main/java/example/lohnsoftware/infrastructure/JsonArbeitszeitkonto.java
+++ src/main/java/example/lohnsoftware/infrastructure/JsonArbeitszeitkonto.java
@@ -1,18 +1,19 @@
 package example.lohnsoftware.infrastructure;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import example.lohnsoftware.core.Arbeitsstunden;
 import example.lohnsoftware.core.Arbeitszeitkonto;
 import example.lohnsoftware.core.MonatsArbeitsstunden;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.Locale;
 
 public class JsonArbeitszeitkonto implements Arbeitszeitkonto {
+
+    private final ObjectMapper objectMapper;
 
     public static Path relativerPfadZumSpeicherort(MonatsArbeitsstunden monatsArbeitsstunden) {
         final var localMonth = monatsArbeitsstunden.month();
@@ -29,25 +30,35 @@ public class JsonArbeitszeitkonto implements Arbeitszeitkonto {
 
     public JsonArbeitszeitkonto(Path basisPfad) {
         this.basisPfad = basisPfad;
+        objectMapper = new ObjectMapper();
     }
 
     @Override
     public Ergebnis erfasse(MonatsArbeitsstunden monatsArbeitsstunden) {
         try {
-            var json = """
-                    {
-                      "stunden": 27,
-                      "minuten": 12
-                    }""";
-            final var arbeitsstunden = monatsArbeitsstunden.arbeitsstunden();
-            json = new ObjectMapper().writeValueAsString(new StorageFormat(arbeitsstunden.stunden().wert(), arbeitsstunden.minuten().wert()));
-            var datei = basisPfad.resolve(relativerPfadZumSpeicherort(monatsArbeitsstunden));
-            Files.createDirectories(datei.getParent());
-            Files.writeString(datei, json, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
-            return Ergebnis.erfolg("Informationen die der Aufrufer im Erfolgsfall braucht");
+            final var json = erstelleJsonFür(monatsArbeitsstunden);
+            var datei = pfadZurDatei(monatsArbeitsstunden);
+            return lockeDateiUndSchreibe(datei, json);
         } catch (IOException e) {
             e.printStackTrace();
             return Ergebnis.fehlschlag("Informationen die der Aufrufer im Falle eines Fehlschlags braucht");
         }
+    }
+
+    private static Ergebnis lockeDateiUndSchreibe(Path datei, String json) throws IOException {
+        Files.createDirectories(datei.getParent());
+        Files.writeString(datei, json, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+        return Ergebnis.erfolg("Informationen die der Aufrufer im Erfolgsfall braucht");
+    }
+
+    private Path pfadZurDatei(MonatsArbeitsstunden monatsArbeitsstunden) {
+        return basisPfad.resolve(relativerPfadZumSpeicherort(monatsArbeitsstunden));
+    }
+
+    private String erstelleJsonFür(MonatsArbeitsstunden monatsArbeitsstunden) throws JsonProcessingException {
+        final var arbeitsstunden = monatsArbeitsstunden.arbeitsstunden();
+        final var stunden = arbeitsstunden.stunden().wert();
+        final var minuten = arbeitsstunden.minuten().wert();
+        return objectMapper.writeValueAsString(new StorageFormat(stunden, minuten));
     }
 }
