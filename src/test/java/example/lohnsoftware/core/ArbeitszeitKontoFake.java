4c4ccbfd7677064ef464b4083ffda6b6e35a4ba4
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-07-16 13:39:54 +0200

    lösche nicht verwendeten code

--- src/test/java/example/lohnsoftware/core/ArbeitszeitKontoFake.java
+++
@@ -1,8 +0,0 @@
-package example.lohnsoftware.core;
-
-class ArbeitszeitKontoFake implements Arbeitszeitkonto {
-    @Override
-    public Ergebnis erfasse(MonatsArbeitsstunden monatsArbeitsstunden) {
-        return null;
-    }
-}
