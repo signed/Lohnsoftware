def0f2a5b82c40b73ad59c0ccae4d124955d02f6
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-12-15 22:41:18 +0100

    use ul

--- src/main/java/example/lohnsoftware/core/MonatsArbeitsstunden.java
+++ src/main/java/example/lohnsoftware/core/MonatsArbeitsstunden.java
@@ -2,9 +2,11 @@ package example.lohnsoftware.core;
 
 /**
  * Warum nur einmal Arbeitsstunden für den ganzen Monat an Stelle von einmal Arbeitsstunden für jeden Tag
- * - Meiner Einschätzung nach werden die Tagesinformation zur Lohnzahlung nicht gebrauch (könnte falsch sein)
- * - Tages genaue Arbeitsstunden stehen in der Zeiterfassung, warum in die Lohnsoftware duplizieren
- * - Einfacher zu implementieren
+ * <ul>
+ *     <li>Meiner Einschätzung nach werden die Tagesinformation zur Lohnzahlung nicht gebrauch (könnte falsch sein)</li>
+ *     <li>Tages genaue Arbeitsstunden stehen in der Zeiterfassung, warum in die Lohnsoftware duplizieren</li>
+ *     <li>Einfacher zu implementieren</li>
+ * </ul>
  */
 public record MonatsArbeitsstunden(LocalMonth month, Mitarbeiter mitarbeiter, Arbeitsstunden arbeitsstunden) {
 }
