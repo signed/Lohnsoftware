02deb42f7d655c554987aabcb54061916ddee9d7
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-04-04 22:03:02 +0200

    remove dev tools

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     java
     id("org.springframework.boot") version "3.4.1"
-    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "example"
@@ -18,12 +17,12 @@ repositories {
 }
 
 dependencies {
+    implementation(platform(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES))
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("io.vavr:vavr:0.10.5")
     implementation("org.assertj:assertj-vavr:0.4.3")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
