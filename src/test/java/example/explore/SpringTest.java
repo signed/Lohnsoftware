ef34a0798c67aef31ef6c22571ef97e7c5f7e28e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-05-14 22:20:20 +0200

    sample for optional configuration

--- src/test/java/example/explore/SpringTest.java
+++ src/test/java/example/explore/SpringTest.java
@@ -2,9 +2,12 @@ package example.explore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class SpringTest {
@@ -20,8 +23,11 @@ public class SpringTest {
   @Autowired
   private String hello;
 
+  @Value("${org.example.value}")
+  private String value;
+
   @Test
   void name() {
-
+    assertThat(value).isEqualTo("Hello");
   }
 }
