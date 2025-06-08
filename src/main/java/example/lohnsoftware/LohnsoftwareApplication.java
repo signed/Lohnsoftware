fb7dd919a631ee4e31879e3627fe6dc1a10987ca
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-06-08 12:33:24 +0200

    make option to use java configuration more obvious

--- src/main/java/example/lohnsoftware/LohnsoftwareApplication.java
+++ src/main/java/example/lohnsoftware/LohnsoftwareApplication.java
@@ -2,6 +2,7 @@ package example.lohnsoftware;
 
 import example.lohnsoftware.cron.CronKonfiguration;
 import example.lohnsoftware.http.SpringSecurityKonfiguration;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -11,7 +12,9 @@ import org.springframework.context.annotation.Import;
 public class LohnsoftwareApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LohnsoftwareApplication.class, args);
+      var application = new SpringApplication(LohnsoftwareApplication.class);
+      application.setBannerMode(Banner.Mode.OFF);
+      application.run(args);
     }
 
 }
