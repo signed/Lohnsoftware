1ae458cfca4e073411850f2e7127890fc2fe84cc
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2025-08-11 12:35:11 +0200

    nested

--- src/main/java/example/lohnsoftware/explore/UseFluentApi.java
+++ src/main/java/example/lohnsoftware/explore/UseFluentApi.java
@@ -6,6 +6,8 @@ import org.slf4j.MDC;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
 // https://www.slf4j.org/manual.html#fluent
 class UseFluentApi implements CommandLineRunner {
@@ -13,7 +15,9 @@ class UseFluentApi implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-      LOGGER.atInfo().setMessage("the fluent way").addKeyValue("three", "fluent").log();
+      LOGGER.atInfo().setMessage("the fluent way 1").addKeyValue("three", "fluent").log();
       LOGGER.atInfo().setMessage("the fluent way 2").log();
+      var one = Map.of("one", 1, "two", 2, "deep", Map.of("sea", "value"));
+      LOGGER.atInfo().setMessage("the fluent way 3").addKeyValue("json", one).log();
     }
 }
