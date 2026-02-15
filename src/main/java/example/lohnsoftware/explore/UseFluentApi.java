6f11badbbb3119ebbb8e596a947276c5ec151452
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2026-02-15 12:01:19 +0100

    remove unused import

--- src/main/java/example/lohnsoftware/explore/UseFluentApi.java
+++ src/main/java/example/lohnsoftware/explore/UseFluentApi.java
@@ -2,7 +2,6 @@ package example.lohnsoftware.explore;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
